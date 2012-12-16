41bea8c3a2a7590be1c1cae3e3188f262b9f11c8
Arnost Valicek <arnost.valicek@gmail.com>
2012-12-16 22:41:03 +0100

    Rename script which runs test suite.

--- src/runTestSuite.groovy
+++ src/runTestSuite.groovy
@@ -1,11 +1,10 @@
 def testDataDir = new File(".").getCanonicalFile()
-println "Reading test config in " + testDataDir
 
-
+println "Reading test suite from ${testDataDir}"
 def reader = new TestDefinitionReader(testDataDir)
 
 TestSuite suite = reader.readTestSuite()
 
-
+println "Running test suite"
 TestRunner runner = new TestRunner()
 runner.runTestSuite(suite)
