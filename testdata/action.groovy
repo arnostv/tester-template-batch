ad8b2c15d3634f27c1baa57a82bee332418a785a
Arnost Valicek <arnost.valicek@gmail.com>
2012-12-17 19:53:09 +0100

    Use Grape to define classpath

--- testdata/action.groovy
+++ testdata/action.groovy
@@ -1,4 +1,13 @@
+@Grab(group='org.springframework', module='spring', version='2.5.6')
+import org.springframework.jdbc.core.JdbcTemplate
+
+@GrabConfig(systemClassLoader=true)
+@Grab(group='hsqldb', module='hsqldb', version='1.8.0.10')
+import org.hsqldb.jdbcDriver
+
 println "Default test suite action for test - uppercase it"
+
+def driverClass = Class.forName("org.hsqldb.jdbcDriver")
 
 def utils = new TestingUtils()
 utils.hello()
